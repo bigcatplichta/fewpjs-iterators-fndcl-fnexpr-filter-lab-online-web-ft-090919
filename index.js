d8406213ce18c5b6e4dbce22bfceb5b68d1117da
Matt Plichta <bigcatplichta@gmail.com>
2020-01-20 22:47:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-function findMatching(list, search)
+function findMatching(list, search) {
+  
+}
+
+function findMatching(a, b) {
+    return a.filter(possibleMatch => 
+        possibleMatch.toLowerCase() === b.toLowerCase()
+     )
+}
