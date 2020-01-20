7d4debfe3f341b23a217a80fc631209c2083dee8
Matt Plichta <bigcatplichta@gmail.com>
2020-01-20 22:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function findMatching(list, searchTerm) {
   let result = list.filter(match =>
+  // convert searchTerm and match to lower case
   match.toLowerCase() === searchTerm.toLowerCase()
   )
   return result
 }
 
-function findMatching(a, b) {
-    return a.filter(possibleMatch => 
-        possibleMatch.toLowerCase() === b.toLowerCase()
-     )
+function fuzzyMatch(list, searchTerm) {
+  
 }
